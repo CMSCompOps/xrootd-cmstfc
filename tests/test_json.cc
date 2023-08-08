a631f77100161693b046b4f1f7cb2fda38a4f5e5
guyzsarun <sarun.nunt@gmail.com>
2023-08-08 10:27:49 +0200

    add tests

---
+++ tests/test_json.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <sys/param.h>
+#include "src/json/XrdCmsJson.hh"
+#include "XrdSys/XrdSysError.hh"
+#include "XrdSys/XrdSysLogger.hh"
+
+
+int main (int, const char** argv)
+{
+   const char* lfn = argv[1];
+   const char *rf = argv[2];
+
+   int blen = 4096;
+   char* buff = (char*) malloc(blen);
+   XrdSysLogger myLogger;
+   XrdSysError eDest(&myLogger, "tfc_");
+   eDest.Say("TFC Module");
+   XrdCmsJson::PathTranslation *cmsJson = new XrdCmsJson::PathTranslation (&eDest, rf);
+   cmsJson->lfn2pfn(lfn, buff, blen);
+   std::cout << buff << std::endl;
+   return 0;
+}
